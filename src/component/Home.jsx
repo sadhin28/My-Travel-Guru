bc6b991cffe14370a9bf9b05d09296d4bc40aa1f
sadhin28 <taosifsadhin113@gmail.com>
2025-06-09 16:56:39 +0600

    add nav bar

---
+++ src/component/Home.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+
+const Home = () => {
+    return (
+        <div>
+           <header>
+              <NavBar></NavBar>
+           </header>
+        </div>
+    );
+};
+
+export default Home;
